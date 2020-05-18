1ca3619d2330d2248ff7242b628db06a62f2b425
rr <roznoshchik@gmail.com>
2020-05-18 15:53:18 +0300

    added google auth login

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,15 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from flask import request
 from werkzeug.urls import url_parse
+import requests
+from oauthlib.oauth2 import WebApplicationClient
+import json
+
+client = WebApplicationClient(app.config['GOOGLE_CLIENT_ID'])
+def get_google_provider_cfg():
+    return requests.get(app.config['GOOGLE_DISCOVERY_URL']).json()
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -21,9 +30,11 @@ def index():
         }
     ]
     
-    
     return render_template('index.html', title = 'Home', posts=posts)
-    
+
+if __name__ == "__main__":
+    app.run(ssl_context=('cert.pem', 'key.pem'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -46,6 +57,80 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/goog')
+def goog():
+    
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope = ["openid", "email", "profile"],
+        prompt="select_account"
+    )
+    return redirect(request_uri)
+
+@app.route("/goog/callback")
+def callback():
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(app.config['GOOGLE_CLIENT_ID'], app.config['GOOGLE_CLIENT_SECRET'],)
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+    
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in your db with the information provided
+    # by Google
+    newuser = User(googid=unique_id, email=users_email, username=users_name)
+    user = User.query.filter_by(googid=unique_id).first()    
+    if user is None:
+        db.session.add(newuser)
+        db.session.commit()
+        user = User.query.filter_by(username=users_email).first()
+            
+    login_user(user)
+    return redirect(url_for("index"))
+
+
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -59,3 +144,4 @@ def register():
         flash('Congratulations, you are now a registered user!', 'message')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
